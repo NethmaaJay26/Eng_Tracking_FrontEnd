5f20f2a4195e98405230f3835b90793c2b2b453c
Nethma <nethmade@gmail.com>
2024-06-14 22:44:53 +0530

    Homebanner2

--- src/Components/BannerHome/BannerHome.jsx
+++ src/Components/BannerHome/BannerHome.jsx
@@ -5,11 +5,12 @@ import pointImage1 from '../Assets/bell_icon.png'
 import pointImage2 from '../Assets/bell_icon.png'
 import pointImage3 from '../Assets/bell_icon.png'
 import pointImage4 from '../Assets/bell_icon.png'
+import pointImage5 from '../Assets/bell_icon.png'
 
 const Banner = () => {
   const navigate = useNavigate();
 
-  const ClickTea = () => {
+  const ClickThis = () => {
     navigate('/Trainings');
   }
 
@@ -20,27 +21,32 @@ const Banner = () => {
           <img src={pointImage1} alt="Point 1" />
           <h3>Practical and Technical
           engineering knowledge</h3>
-          <p>Our advanced spyware detection engine can identify if a device contains spyware or bugging software.</p>
+          <p>Our advanced spyware detection engine can identify if a device contains spyware or bugging software</p>
         </div>
         <div className="point">
           <img src={pointImage2} alt="Point 2" />
           <h3>Managerial Involvement </h3>
-          <p>Our advanced spyware detection engine can identify if a device contains spyware or bugging software.</p>
+          <p>Our advanced spyware detection engine can identify if a device contains spyware or bugging software</p>
         </div>
         <div className="point">
           <img src={pointImage3} alt="Point 3" />
           <h3>Interpersonal skills</h3>
-          <p>Our advanced spyware detection engine can identify if a device contains spyware or bugging software.</p>
+          <p>Our advanced spyware detection engine can identify if a device contains spyware or bugging software</p>
         </div>
         <div className="point">
           <img src={pointImage4} alt="Point 4" />
           <h3>Professional conduct</h3>
-          <p>Our advanced spyware detection engine can identify if a device contains spyware or bugging software.</p>
+          <p>Our advanced spyware detection engine can identify if a device contains spyware or bugging software</p>
         </div>
+        <div className="point">
+          <img src={pointImage5} alt="Point 5" />
+          <h3>Others</h3>
+          <p>Our advanced spyware detection engine can identify if a device contains spyware or bugging software</p>
+        </div>
+        <div className="buttons-container">
+        <button className='button1' onClick={ClickThis} >Structured Training Programs</button>
+        <button className= 'button2' onClick={() => navigate('/Training')}>Upcoming Training Programs</button>
       </div>
-      <div className="buttons-container">
-        <button onClick={ClickTea}>Explore New Products</button>
-        <button onClick={() => navigate('/another-page')}>Another Button</button>
       </div>
     </div>
   )
