a671f0a6ad43a929a4aa6255abb5664950bdb14e
Nethma <nethmade@gmail.com>
2024-07-09 23:11:04 +0530

    hr dashboard

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import BannerHome from '../Components/BannerHome/BannerHome'
-import BannerHome2 from '../Components/BannerHome2/BannerHome2'
-
 
 
 export const Home = () => {
   return (
     <div>
       <BannerHome/>
-      <BannerHome2/>
+     
 
     </div>
   )
