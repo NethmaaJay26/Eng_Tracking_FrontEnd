610fc7678cd2b00ef126cf6f62f0045849f8f2ec
Oshadi <oshadinethmi27@gmail.com>
2024-10-24 10:47:28 +0530

    CSS

--- src/Components/TopBar/Topbar.jsx
+++ src/Components/TopBar/Topbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Topbar.css';
-import bell_icon from '../Assets/bell_icon.png';
+
 import logoLTL from '../Assets/logoLTL.png';
 
 export default function Topbar() {
@@ -18,7 +18,7 @@ export default function Topbar() {
   return (
     <div className='topbar'>
       <div className="topbar-logo">
-        <img src={logoLTL} alt="Logo" /> 
+
         <p>ENGINEERINGProTrack</p>
         <img src={logoLTL} alt="Logo" />
        
